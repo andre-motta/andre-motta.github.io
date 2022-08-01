bf11b29b739964ee0a18143161c3faf4816c819b
zxcv123456qwe <89665004+zxcv123456qwe@users.noreply.github.com>
2022-08-01 15:54:50 -0400

    upgrade website test

---
+++ precache-manifest.f49fb5ea3c68301e950a71c2d400c5ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d4368a1cfcaa07d175b7ebb46de852",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b1c78012b0355050d49",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5f64366b52019a2288b3",
+    "url": "/static/css/main.fcf2461e.chunk.css"
+  },
+  {
+    "revision": "4b1c78012b0355050d49",
+    "url": "/static/js/2.6b446f1b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.6b446f1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f64366b52019a2288b3",
+    "url": "/static/js/main.01fe5f77.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
